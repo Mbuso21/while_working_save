143527d43cc92a6d14513411155a68a2b808efc4
mmehloma <mmehloma@student.wethinkcode.za.co>
2021-04-07 15:24:17 +0200

    added mazerunner.py

---
+++ mazerunner.py
@@ -0,0 +1,21 @@
+
+def run(maze):
+    '''
+    runs to the last value of the list and returns the path
+    '''
+    no_go = []
+    visited = []
+    pace = []
+
+    
+
+    return visited
+
+
+if __name__ == "__main__":
+    maze = [[1,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [0,0,1,1,0,0,0,0,1,0,0,0,0,0,0],
+            [0,0,0,1,0,0,0,0,1,1,1,1,1,1,1]]
+    
+    run(maze)
